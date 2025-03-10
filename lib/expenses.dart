85d99e59bd291f7c2c0beff64440e9ff896b6aad
nurislamilyasov <nurikileasov0000@gmail.com>
2025-03-10 19:55:07 +0500

    first commit

---
+++ lib/expenses.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+import 'models/expense.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() => _ExpensesState();
+}
+
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(title: 'Flutter Course',
+        amount: 19.99,
+        date: DateTime.now(),
+        category: Category.work),
+    Expense(title: 'Flutter Course',
+        amount: 19.99,
+        date: DateTime.now(),
+        category: Category.work),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Text('The chart'),
+          Text('Expenses list'),
+        ],
+      ),
+    );
+  }
+}
